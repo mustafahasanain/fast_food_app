5f0e9598c3791b64f4b21b51c1050f5b4caa98b1
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-06 19:54:15 +0300

    Implementing cards to homescreen (without styling)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,10 +7,10 @@ import "./globals.css";
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
-    // "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
-    // "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
-    // "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
-    // "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
   });
 
   useEffect(() => {
