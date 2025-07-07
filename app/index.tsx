8c82c5516b0a5d4f430f9ed4e6b7e619b129e96e
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-07 14:49:52 +0300

    Homepage UI done

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,25 @@
 import { images, offers } from "@/constants";
 import cn from "clsx";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import CartButton from "@/components/CartButton";
 import { Fragment } from "react";
 import "./globals.css";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
+      {/* this for make the full screen scroll */}
+      {/* <ScrollView> - Never wrap FlatList with ScrollView */}
+
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
@@ -56,7 +67,25 @@ export default function Index() {
           );
         }}
         contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELEVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Iraq</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
       />
+      {/* </ScrollView> */}
     </SafeAreaView>
   );
 }
