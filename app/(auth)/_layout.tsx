40a5c571340ad8e8a63484220753ffb0f1dae8df
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-09 11:52:43 +0300

    Create a custom button and custom input components

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,50 @@
+import { CustomButton, CustomInput } from "@/components";
+import { images } from "@/constants";
 import { Slot } from "expo-router";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 export default function _layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth layout</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+
+        <CustomInput
+          placeholder="Enter your email"
+          value={""}
+          onChangeText={(text) => {}}
+          label="Email"
+          keyboardType="email-address"
+        />
+        <CustomButton />
+      </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   Platform: "com.mustafahasanain.fastfood",
+  databaseId: "686cfd7000147a1dbd68",
+  userCollectionId: "686cfdb00033844b0246",
 };
